39518c7cd90415099ea5e725143f1c3c5b6809bd
Shristi Shrestha <shristi0702@gmail.com>
2024-08-08 16:21:34 +0545

    created task list model and make migrations

--- Todo/Todo/settings.py
+++ Todo/Todo/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #own
-    'user',
+    'tasks',
+    'jazzmin',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'Todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
